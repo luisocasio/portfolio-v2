32e56f604e7fb195b29b0c601e57f02aa0a1a18c
Luis <ocasio.dev@gmail.com>
2020-07-11 15:07:54 -0400

    starting carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,30 +1,38 @@
-import React from 'react'
+import React from "react";
+import back from "../assets/luluport_SVG/back.svg";
+import next from "../assets/luluport_SVG/next.svg";
 
 const Carousel = () => {
   return (
-    <div className='carousel_container'>
-      <div className='carousel'>
+    <div className="carousel_container">
+      <div className="back">
+        <img src={back} alt="back arrow" />
+      </div>
+      <div className="carousel">
         Title of Project
         {/* projects go here */}
-        <div className='project_buttons'>
-          <button className='code'>CODE</button>
-          <button className='view'>VIEW</button>
+        <div className="project_buttons">
+          <button className="code">CODE</button>
+          <button className="view">VIEW</button>
         </div>
       </div>
 
-      <div className='details'>
-        <div className='project_details'>
+      <div className="details">
+        <div className="project_details">
           <h3>About this Project</h3>
           <p>8 week long project</p>
         </div>
-        <div className='stack_details'>
+        <div className="stack_details">
           <h3>Tech Stack</h3>
           <p>React</p>
           <p>Javascript</p>
         </div>
       </div>
+      <div className="next">
+        <img src={next} alt="forward arrow" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import IntroContent from '../components/IntroContent'
-import Projects from '../components/Projects'
-import About from '../components/About'
-import IntroTitle from '../components/IntroTitle'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import IntroContent from "../components/IntroContent";
+import About from "../components/About";
+import IntroTitle from "../components/IntroTitle";
+import Carousel from "../components/Carousel";
 
 const Landing = () => {
   return (
@@ -12,11 +12,11 @@ const Landing = () => {
       <Header />
       <IntroContent />
       <IntroTitle />
-      <Projects />
+      <Carousel />
       <About />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
