f7c97d37525a8cf3f149ae93beb9c748d8979943
Luis <ocasio.dev@gmail.com>
2020-07-11 17:14:10 -0400

    add icons

--- src/components/IntroTitle.js
+++ src/components/IntroTitle.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { FaHtml5, FaReact, FaJs } from "react-icons/fa";
 
 const IntroTitle = () => {
   return (
     <div className="intro_title">
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
+      <div>
+        <FaHtml5 />
+      </div>
+      <div>
+        <FaReact />
+      </div>
+      <div>
+        <FaJs />
+      </div>
     </div>
   );
 };
